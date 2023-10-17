68ab4dece91116f5966126c4e0aa3f66de8aa410
sajadwzn <82923359+sajadwzn@users.noreply.github.com>
2023-10-16 23:43:11 -0400

    Fixed function signature bug

--- src/app/lexer/typescript/typescript.statemachine.ts
+++ src/app/lexer/typescript/typescript.statemachine.ts
@@ -1,9 +1,18 @@
+import { ASTNode } from "src/app/types/astnode.type";
+import { LineElement } from "src/app/types/lineelement.type";
 import { State } from "src/app/types/state.type";
 
 export class TypeScriptStateMachine {
 
-    parseState(s: State) {
-        
+    parseState(options?: {
+        state?: State,
+        callback?: Function
+    }): {
+        run?: Function,
+        node?: ASTNode,
+        element?: LineElement
+    } | undefined
+    {
+        return undefined
     }
-
 }
